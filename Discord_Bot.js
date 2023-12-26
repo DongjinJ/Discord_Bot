bb6541bd9fb17c8c0de8fb114ca5931641419dc5
동진 <42710922+DongjinJ@users.noreply.github.com>
2023-12-26 23:39:06 +0900

    [feat]: [OpenAPI] Open API 기반 가치 추정 기능
    1. 기능 안정화
    : Lost Ark API Get/Post 함수 통일
    2. 알림 문구 수정
    
    -#7

--- Discord_Bot.js
+++ Discord_Bot.js
@@ -7,21 +7,22 @@ const fs = require('fs')
 const schedule = require('node-schedule');
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-// import { getPrice } from './OpenApi';
-const lostarkApi = require('./OpenApi.js')
-const ancientRelicsId = 6882701;
-const rareRelicsId = 6882704;
-const orehaRelicsId = 6885708;
-const targetStoneId = 6861011;
-
+const fetch = require('fetch');
 require("dotenv").config();
 
 const { spawn } = require('child_process');
 var Iconv  = require('iconv').Iconv;
 var iconv = Iconv('EUC-KR', 'UTF-8')
 
-const testMode = true;
+// import { getPrice } from './OpenApi.js';
+// const lostarkApi = require('./OpenApi.js')
+const LostArk_Api = process.env.LOSTARK_API_KEY;
+const ancientRelicsId = 6882701;
+const rareRelicsId = 6882704;
+const orehaRelicsId = 6885708;
+const targetStoneId = 6861011;
+
+const testMode = false;
 var serviceChannelID = '';
 if(testMode === true){
     serviceChannelID = process.env.TEST_SERVER_ID;
@@ -30,6 +31,62 @@ else{
     serviceChannelID = process.env.CLIENT_SERVER_ID;
 }
 
+async function getData(url="", itemID="", token="",data = {}) {
+    // console.log(url)
+    // console.log(itemID)
+    // console.log(token)
+    
+    const response = await axios.get(url+itemID, {
+        headers: {
+            "accpet": "application/json",
+            "authorization": "bearer " + token,
+        }
+    })
+    // console.log(response);
+    return response.data;
+};
+
+async function getPrice(itemID, token, data={}) {
+    // console.log('Get Price')
+    await getData("https://developer-lostark.game.onstove.com/markets/items/", itemID, token).then((data) => {
+        // console.log(data[0].Stats);
+        itemName = data[0].Name;
+        
+        var today = new Date();
+    
+        var year = today.getFullYear();
+        var month = ('0' + (today.getMonth() + 1)).slice(-2);
+        var day = ('0' + today.getDate()).slice(-2);
+    
+        var dayString = year + '-' + month + '-' + day;
+    
+        for(i = 0; i < data[0].Stats.length;i++){
+            if(data[0].Stats[i].Date == dayString){
+                // console.log(data[0].Stats[i].Date);
+                // console.log(data[0].Stats[i].AvgPrice);
+                itemPrice = data[0].Stats[i].AvgPrice;
+            }
+            
+        }
+        itemBundle = data[0].BundleCount;
+    })
+
+    let value = {Name: itemName, Price: itemPrice, Bundle: itemBundle};
+    return value;
+    
+}
+
+async function postData(url="", data = {}) {
+    const response = await fetch(url+data, {
+        method: "POST",
+        headers: {
+            "accpet": "application/json",
+            "authorization": "bearer " + LostArk_Api,
+        }
+    })
+
+    return response.json();
+};
 
 var updateData = schedule.scheduleJob('0 6 ? * 3', function(){
     const dataBuffer = fs.readFileSync('party.json')
@@ -104,41 +161,50 @@ client.on('messageCreate', async(msg) => {
                             var answerData = '';
                             answerData += '- 입력 유물 재료' + '\n';
                             answerData += `  오레하 유물: ${split_str[1]} / 희귀한 유물: ${split_str[2]} / 고대 유물: ${split_str[3]}` + '\n';
-                            answerData += `- 교환 유물 재료` + '\n';
-                            answerData += `  오레하 유물: ${targetH} / 희귀한 유물: ${targetM} / 고대 유물: ${targetL}` + '\n\n';
                             answerData += `* 희귀한 유물 -> 고고학 가루: ${maxMIndex}개 (${maxMIndex * 50})` + '\n';
                             answerData += `* 고대 유물 -> 고고학 가루: ${maxLIndex}개 (${maxLIndex * 100})` + '\n';
                             answerData += `* 고고학 가루 -> 오레하 유물: ${maxPowder}개 (${(maxPowder / 10) - (maxPowder % 10)})` + '\n\n';
                             answerData += `- 변환 후 유물 재료` + '\n';
                             answerData += `  오레하 유물: ${finalH} / 희귀한 유물: ${finalM} / 고대 유물: ${finalL}` + '\n';
-                            answerData += `  현재 만들 수 있는 최상급 오레하: ${cost}개` + '\n';
+                            answerData += `  현재 만들 수 있는 최상급 오레하: ${cost}개` + '\n\n';
 
-                            lostarkApi.getPrice(ancientRelicsId).then((itemValue) => {
+                            getPrice(ancientRelicsId, LostArk_Api).then((itemValue) => {
                                 ancientRelicsData = itemValue;
-                            });
-                            lostarkApi.getPrice(rareRelicsId).then((itemValue) => {
-                                rareRelicsData = itemValue;
-                            });
-                            lostarkApi.getPrice(orehaRelicsId).then((itemValue) => {
-                                orehaRelicsData = itemValue;
-                            });
-                            lostarkApi.getPrice(targetStoneId).then((itemValue) => {
-                                targetStoneData = itemValue;
-                            });
+                                // console.log(ancientRelicsData.Name + ' / ' + ancientRelicsData.Price)
+                                getPrice(rareRelicsId, LostArk_Api).then((itemValue) => {
+                                    rareRelicsData = itemValue;
+                                    // console.log(rareRelicsData.Name + ' / ' + rareRelicsData.Price)
+                                    getPrice(orehaRelicsId, LostArk_Api).then((itemValue) => {
+                                        orehaRelicsData = itemValue;
+                                        // console.log(orehaRelicsData.Name + ' / ' + orehaRelicsData.Price)
+                                        getPrice(targetStoneId, LostArk_Api).then((itemValue) => {
+                                            targetStoneData = itemValue;
+                                            // console.log(targetStoneData.Name + ' / ' + targetStoneData.Price)
 
-                            while(true){
-                                if ((ancientRelicsData != null) && (rareRelicsData != null) && (orehaRelicsData != null) && (targetStoneData != null)){
-                                    totalAncient = (split_str[3] / ancientRelicsData.Bundle) * ancientRelicsData.Price;
-                                    totalRare = (split_str[2] / rareRelicsData.Bundle) * rareRelicsData.Price;
-                                    totalOreha = (split_str[1] / orehaRelicsData.Bundle) * orehaRelicsData.Price;
-                                    beforeCost = totalAncient + totalRare + totalOreha;
-                                    totalTarget = (cost * 15) * ancientRelicsData.Price;
-                                    answerData += `  ${totalAncient} + ${totalRare} + ${totalOreha} = ${beforeCost} / ${totalTarget} \n`;
-                                    break;
-                                }
-                            }
+                                            totalAncient = Math.floor(split_str[3] / ancientRelicsData.Bundle) * ancientRelicsData.Price;
+                                            totalRare = Math.floor(split_str[2] / rareRelicsData.Bundle) * rareRelicsData.Price;
+                                            totalOreha = Math.floor(split_str[1] / orehaRelicsData.Bundle) * orehaRelicsData.Price;
+                                            beforeCost = totalAncient + totalRare + totalOreha;
+                                            totalTarget = ((cost * 15) * targetStoneData.Price) - (cost * 300);
 
-                            msg.reply(answerData);
+                                            answerData += `  < 금일 평균가 기준 >\n`;
+                                            answerData += `- 고대 유물: ${split_str[3]} / ${ancientRelicsData.Bundle} * ${ancientRelicsData.Price} = ${totalAncient}\n`;
+                                            answerData += `- 희귀한 유물: ${split_str[2]} / ${rareRelicsData.Bundle} * ${rareRelicsData.Price} = ${totalRare}\n`;
+                                            answerData += `- 오레하 유물: ${split_str[1]} / ${orehaRelicsData.Bundle} * ${orehaRelicsData.Price} = ${totalOreha}\n`;
+                                            answerData += `  순수 재료 값: ${beforeCost}\n`;
+                                            answerData += `- 최상급 오레하: (${cost} * 15 * ${targetStoneData.Price}) - (${cost} * 300) = ${totalTarget}\n`;
+
+
+                                            msg.reply(answerData);
+                                        });
+                                    });
+                                    
+                                });
+                                
+                            });
+                        
+
+                            
                         }
                         else{
                             msg.reply('[Error]: 계산 오류. 다시 시도해주세요.')
@@ -533,5 +599,6 @@ client.on('interactionCreate', async interaction => {
         fs.writeFileSync('party.json', saveJSON)
     }
 });
+
 
 client.login(process.env.DISCORD_TOKEN);
--- OpenApi.js
+++ OpenApi.js
@@ -1,28 +1,35 @@
+// import fetch from "node-fetch";
+const fetch = require('fetch');
+const axios = require('axios');
 
-require("dotenv").config();
-LostArk_Api = process.env.LOSTARK_API_KEY;
 
-ancientRelicsId = 6882701;
-rareRelicsId = 6882704;
-orehaRelicsId = 6885708;
-targetStoneId = 6861011;
-
-async function getData(url="", itemID="",data = {}) {
+async function getData(url="", itemID="", token="",data = {}) {
+    // console.log(url)
+    // console.log(itemID)
+    // console.log(token)
     
-    const response = await fetch(url+itemID, {
-        method: "GET",
+    // const response = await fetch(url+itemID, {
+    //     method: "GET",
+    //     headers: {
+    //         "accpet": "application/json",
+    //         "authorization": "bearer " + token,
+    //     }
+    // })
+
+    const response = await axios.get(url+itemID, {
         headers: {
             "accpet": "application/json",
-            "authorization": "bearer " + LostArk_Api,
+            "authorization": "bearer " + token,
         }
     })
-
-    return response.json();
+    // console.log(response);
+    return response.data;
 };
 
-async function getPrice(itemID, data={}) {
-    
-    await getData("https://developer-lostark.game.onstove.com/markets/items/", itemID).then((data) => {
+async function getPrice(itemID, token, data={}) {
+
+    await getData("https://developer-lostark.game.onstove.com/markets/items/", itemID, token).then((data) => {
+        // console.log(data);
         itemName = data[0].Name;
         
         var today = new Date();
@@ -35,8 +42,8 @@ async function getPrice(itemID, data={}) {
     
         for(i = 0; i < data[0].Stats.length;i++){
             if(data[0].Stats[i].Date == dayString){
-                // console.log(data[0].Stats[i].Date);
-                // console.log(data[0].Stats[i].AvgPrice);
+                console.log(data[0].Stats[i].Date);
+                console.log(data[0].Stats[i].AvgPrice);
                 itemPrice = data[0].Stats[i].AvgPrice;
             }
             
@@ -61,23 +68,39 @@ async function postData(url="", data = {}) {
     return response.json();
 };
 
-// itemID = ancientRelicsId;
-// getPrice(itemID).then((itemValue) => {
-//     console.log(itemValue.Name + ': ' + itemValue.Price + ' / ' + itemValue.Bundle);
-// });
+// module.exports = {
+//     getPrice
+// }
+async function orehaTotal(){
+    const ancientRelicsId = 6882701;
+    const rareRelicsId = 6882704;
+    const orehaRelicsId = 6885708;
+    const targetStoneId = 6861011;
+    LostArk_Api="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAzMDAyMzQifQ.dQv88Cpuo9iir719wpUtCUV44uA07uN2d4zegl_MNPWiOfpbVDiNmaZAyy90gdDwBsknZKrdcn26imEQLlDnZg-SgC3VrXuqf2CRyT5G2EUhldNuTDtpZi_RSIYWYCXAhAAM-Ng8eaXQSPws-R36dyw9Ld1CDGkEE1Dih-rZEYoIQ-AQfTLs4we0wSG9TW7y5iW4yrJ5WtaOba3mxCH1chz5WQzJPe4ZFoSktHoMUCmMug0bhJXGLbCVrwlmCbsI1BRWc9SzqAn5rX0PKjeSEkF5mzHK9HXBT6ps9DdSIcS3l214nViDt_JwsOG-l4R95wsT0opR8qeJyRkJjWKq7w"
+    getPrice(ancientRelicsId, LostArk_Api).then((itemValue) => {
+        ancientRelicsData = itemValue;
+        
+    });
+    getPrice(rareRelicsId, LostArk_Api).then((itemValue) => {
+        rareRelicsData = itemValue;
+        
+    });
+    getPrice(orehaRelicsId, LostArk_Api).then((itemValue) => {
+        orehaRelicsData = itemValue;
+        
+    });
+    getPrice(targetStoneId, LostArk_Api).then((itemValue) => {
+        targetStoneData = itemValue;
+        
+    });
+    let value = {ancientRelicsData, rareRelicsData, orehaRelicsData, targetStoneData};
 
-
-// itemID = rareRelicsId;
-// getPrice(itemID).then((itemValue) => {
-//     console.log(itemValue.Name + ': ' + itemValue.Price + ' / ' + itemValue.Bundle);
-// });
-
-// itemID = orehaRelicsId;
-// getPrice(itemID).then((itemValue) => {
-//     console.log(itemValue.Name + ': ' + itemValue.Price + ' / ' + itemValue.Bundle);
-// });
-
-// itemID = targetStoneId;
-// getPrice(itemID).then((itemValue) => {
-//     console.log(itemValue.Name + ': ' + itemValue.Price + ' / ' + itemValue.Bundle);
-// });
+    return value;
+    
+}
+orehaTotal().then((returnValue) => {
+    console.log(returnValue.ancientRelicsData.Name + ' / ' + returnValue.ancientRelicsData.Price)
+    console.log(returnValue.rareRelicsData.Name + ' / ' + returnValue.rareRelicsData.Price)
+    console.log(returnValue.orehaRelicsData.Name + ' / ' + returnValue.orehaRelicsData.Price)
+    console.log(returnValue.targetStoneData.Name + ' / ' + returnValue.targetStoneData.Price)
+})
